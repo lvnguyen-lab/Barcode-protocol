9050766dfa590ddd9cdf5361d9115365c4b35d87
lvnguyen-lab <162927969+lvnguyen-lab@users.noreply.github.com>
2025-10-10 16:39:36 -0400

    Add files via upload

--- Code/amplicon_analysis.sh
+++ Code/amplicon_analysis.sh
@@ -17,6 +17,7 @@
 #           - when this script is called, type "sbatch amplicon_seq_processing_adapted.sh run_name" into the terminal
 #       - ONLY fastq files are in the fastq folder
 #       - data files are in .fastq.gz format
+#       - The reads are paired, so R1 and R2 files are present for each sample 
 #   - Operator folder and script structure:
 #       - the operator has set up a folder for amplicon analysis, in which this shell script is present
 #       - this folder has a folder for "runs" and a folder for "scripts", and these folders are named as such
@@ -26,9 +27,9 @@
 #     - Be aware that all 3 library sequences are tested against each sample, so it is critical to know exactly which library any given sample should belong to
 
 # Setup
-BASE_DIR=/cluster/projects/nguyengroup/
+BASE_DIR=/cluster/projects/nguyengroup/scRNAseq
 WD=$(pwd -P)
-AMP_RUN=$1
+RUN_NAME=$1
 TYPE=$2
 QUALITY=$3
 
@@ -43,7 +44,7 @@ then
   TYPE=$2
   echo ${TYPE}
 else
-  echo "Please enter the type of reads you have want to perform"
+  echo "Please enter whether you want to analyze paired reads together or as unpaired"
   echo "Enter 'paired' for paired reads"
   echo "Enter 'unpaired' for unpaired reads"
   exit 1
@@ -54,13 +55,18 @@ if [[ $3 == "" ]];
     QUALITY=30
 fi
 
-RUN_DIR=${WD}/runs/${AMP_RUN} 
-AMP_DIR=${RUN_DIR}/fastq 
+if [ "$WD" != "$BASE_DIR/runs/$RUN_NAME" ]
+then
+        echo "Error: you should run this from the specific run directory ($BASE_DIR/runs/$RUN_NAME)"
+        exit 1
+fi
 
-mkdir -p ${RUN_DIR}/Cutadapt
-mkdir -p ${RUN_DIR}/Grep
-mkdir -p ${RUN_DIR}/Final_counts
-mkdir -p ${RUN_DIR}/Summary_stats
+AMP_DIR=${WD}/fastq/amplicon 
+
+mkdir -p ${WD}/Cutadapt
+mkdir -p ${WD}/Grep
+mkdir -p ${WD}/Final_counts
+mkdir -p ${WD}/Summary_stats
 
 # Activate environment
 conda init zsh
@@ -68,10 +74,10 @@ source ~/.zshrc
 conda activate NMF_env
 
 # Run Python script to pair R1 and R2 files in the AMP_DIR; saves a CSV file with columns for R1 and R2 that then must be read
-python3 ${WD}/scripts/pair_amplicon_fastqs.py ${AMP_DIR} ${RUN_DIR}
+python3 ${BASE_DIR}/scripts/pair_amplicon_fastqs.py ${AMP_DIR} ${WD}
 
 # Load in paired csv file
-PAIRED_FASTQS=${RUN_DIR}/paired_fastq.csv
+PAIRED_FASTQS=${WD}/paired_fastq.csv
 
 # Load modules
 module load R
@@ -86,32 +92,21 @@ do
   then
   #Run Cutadapt to trim constant regions up and downstream of barcode sequence, and also filter based on size (40bp) and quality (Phred ≥30)
   # Cut and keep barcode sequences only (this sequence works for all staggered primers, and has been adapted to the new barcode libraries made by May and naming conventions of PMCRT seq center) 
-    cutadapt -a TAGAAGGCACAGGTCGACAG...TGTTCTAGACATATGGTCTCTCCTAG -A AGGAGAGACCATATGTCTAGAACA...CTGTCGACCTGTGCCTT --discard-untrimmed -o ${RUN_DIR}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq -p ${RUN_DIR}/Cutadapt/${sample}_R2_trimmed_q${QUALITY}_${TYPE}.fq ${AMP_DIR}/${R1} ${AMP_DIR}/${R2} > ${RUN_DIR}/Summary_stats/${sample}_adapter_trim_${TYPE}.txt
+    cutadapt -a TAGAAGGCACAGGTCGACAG...TGTAAAACGACGGCCAGTG -A GACTCACTGGCCGTCGTTTTACA...CTGTCGACCTGTGCCT --discard-untrimmed -o ${WD}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq -p ${WD}/Cutadapt/${sample}_R2_trimmed_q${QUALITY}_${TYPE}.fq ${AMP_DIR}/${R1} ${AMP_DIR}/${R2} > ${WD}/Summary_stats/${sample}_adapter_trim_${TYPE}.txt
 
-    cutadapt -m 40 -M 40 -q ${QUALITY} -o ${RUN_DIR}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq -p ${RUN_DIR}/Cutadapt/${sample}_rev_q${QUALITY}_${TYPE}.fq ${RUN_DIR}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq ${RUN_DIR}/Cutadapt/${sample}_R2_trimmed_q${QUALITY}_${TYPE}.fq > ${RUN_DIR}/Summary_stats/${sample}_quality_trim_${TYPE}.txt
+    cutadapt -m 40 -M 40 -q ${QUALITY} -o ${WD}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq -p ${WD}/Cutadapt/${sample}_rev_q${QUALITY}_${TYPE}.fq ${WD}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq ${WD}/Cutadapt/${sample}_R2_trimmed_q${QUALITY}_${TYPE}.fq > ${WD}/Summary_stats/${sample}_quality_trim_${TYPE}.txt
   else
-    cutadapt -a TAGAAGGCACAGGTCGACAG...TGTTCTAGACATATGGTCTCTCCTAG --discard-untrimmed -o ${RUN_DIR}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq ${AMP_DIR}/${R1} > ${RUN_DIR}/Summary_stats/${sample}_adapter_trim_${TYPE}.txt
+    cutadapt -a TAGAAGGCACAGGTCGACAG...TGTAAAACGACGGCCAGTG --discard-untrimmed -o ${WD}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq ${AMP_DIR}/${R1} > ${WD}/Summary_stats/${sample}_adapter_trim_${TYPE}.txt
 
-    cutadapt -m 40 -M 40 -q ${QUALITY} -o ${RUN_DIR}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq ${RUN_DIR}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq > ${RUN_DIR}/Summary_stats/${sample}_quality_trim_${TYPE}.txt
+    cutadapt -m 40 -M 40 -q ${QUALITY} -o ${WD}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq ${WD}/Cutadapt/${sample}_R1_trimmed_q${QUALITY}_${TYPE}.fq > ${WD}/Summary_stats/${sample}_quality_trim_${TYPE}.txt
   fi
 
   #Run grep to retrieve the reads that match the expected sequence
-
-  grep -A 2 -B 1 'ATCG..GAT..ATC..GGT..ATA..TGT..AAC..ATCG' ${RUN_DIR}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq | sed -n '/^ATCG/p' > ${RUN_DIR}/Grep/${sample}_BC1_q${QUALITY}_${TYPE}.txt
-
-  grep -A 2 -B 1 'GACC..GAT..ATC..GGT..ATA..TGT..AAC..GACC' ${RUN_DIR}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq | sed -n '/^GACC/p' > ${RUN_DIR}/Grep/${sample}_BC2_q${QUALITY}_${TYPE}.txt
-
-  grep -A 2 -B 1 'TGGT..CTA..TAG..CGA..AAA..TAG..ATC..TGGT' ${RUN_DIR}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq | sed -n '/^TGGT/p' > ${RUN_DIR}/Grep/${sample}_BC3_q${QUALITY}_${TYPE}.txt
+  grep -A 2 -B 1 'ACTG..ATC..GAT..AAA..GGT..AAC..' ${WD}/Cutadapt/${sample}_fwd_q${QUALITY}_${TYPE}.fq | sed -n '/^ATCG/p' > ${WD}/Grep/${sample}_BC1_q${QUALITY}_${TYPE}.txt
 
   # Run Levenshtein.R to group single mismatches
-  echo "Running Levenshtein script on BC1"
-  sbatch Levenshtein.sh ${RUN_DIR}/Grep/${sample}_BC1_q${QUALITY}_${TYPE}.txt ${RUN_DIR}/Final_counts/${sample}_BC1_q${QUALITY}_${TYPE}
-
-  echo "Running Levenshtein script on BC2"
-  sbatch Levenshtein.sh ${RUN_DIR}/Grep/${sample}_BC2_q${QUALITY}_${TYPE}.txt ${RUN_DIR}/Final_counts/${sample}_BC2_q${QUALITY}_${TYPE}
-
-  echo "Running Levenshtein script on BC3"
-  sbatch Levenshtein.sh ${RUN_DIR}/Grep/${sample}_BC3_q${QUALITY}_${TYPE}.txt ${RUN_DIR}/Final_counts/${sample}_BC3_q${QUALITY}_${TYPE}
+  echo "Running Levenshtein script"
+  sbatch ${BASE_DIR}/scripts/Levenshtein.sh ${WD}/Grep/${sample}_BC1_q${QUALITY}_${TYPE}.txt ${WD}/Final_counts/${sample}_BC1_q${QUALITY}_${TYPE}
 
 done < ${PAIRED_FASTQS}
 
